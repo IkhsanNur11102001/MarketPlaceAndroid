761846ceed8bab633a973ca5127ee59e4a9882d7
Nur Ikhsan <ikhsangates6661@gmail.com>
2022-06-10 03:16:14 +0700

    update nav bottom

---
+++ app/src/main/java/com/nur_ikhsan/marketplace/Navigations.kt
@@ -0,0 +1,35 @@
+package com.nur_ikhsan.marketplace
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.nur_ikhsan.marketplace.databinding.ActivityNavigationsBinding
+
+class Navigations : AppCompatActivity() {
+
+    private lateinit var binding: ActivityNavigationsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityNavigationsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_navigations)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/com/nur_ikhsan/marketplace/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.nur_ikhsan.marketplace.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
